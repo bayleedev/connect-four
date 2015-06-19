31e7356687a5e2ea9d445bec4a3ac81538b192b3
Blaine Schmeisser <blainesch@gmail.com>
2015-06-19 12:49:36 -0700

    Refactor possbile_wins method.

--- index.rb
+++ index.rb
@@ -9,13 +9,13 @@ class Board
 
   def initialize
     @pieces = [
-      [:red, :orange, :orange, :white, :orange, :black],
-      [:yellow, nil, :white, nil, nil, nil],
-      [:red, :white, nil, nil, nil, nil],
-      [:white, nil, nil, nil, nil, nil],
+      [:black, :black, :orange, :orange, :black, :black],
+      [:black, nil, :white, :orange, nil, :black],
+      [:red, :white, nil, :orange, nil, nil],
+      [:white, nil, nil, :yellow, nil, nil],
       [:red, nil, nil, nil, nil, nil],
-      [:red, nil, nil, nil, nil, nil],
-      [:red, :yellow, nil, nil, nil, :black]
+      [:black, nil, nil, nil, nil, :black],
+      [:black, :black, nil, nil, :black, :black]
     ]
   end
 
@@ -37,18 +37,20 @@ class Board
   end
 
   def point_won?(row, column)
-    [:n, :w, :nw, :ne].find do |dir|
-      items = possible_win(dir, row, column)
+    possible_wins(row, column).find do |items|
       items.compact.size == 4 and items.uniq.length == 1
     end
   end
 
-  def possible_win(direction, row, column)
-    relative_direction = {n: [-1, 0], w: [0, -1], nw: [-1, -1], ne: [-1, 1]}[direction]
-    (0..3).map do |i|
-      x = row + (relative_direction[0] * i)
-      y = column + (relative_direction[1] * i)
-      pieces[x][y] if x >= 0 and y >= 0
+  def possible_wins(row, column)
+    directions = {n: [-1, 0], w: [0, -1], nw: [-1, -1], ne: [-1, 1]}
+    directions.keys.map do |direction|
+      relative_direction = directions[direction]
+      (0..3).map do |i|
+        x = row + (relative_direction[0] * i)
+        y = column + (relative_direction[1] * i)
+        pieces[x][y] if x >= 0 and y >= 0
+      end
     end
   end
 
